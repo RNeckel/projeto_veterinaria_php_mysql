87c041e2ed4c6bfc3cf4353e2248194a33ef4b82
Ricardo Enio Neckel <neckel214@gmail.com>
2024-11-08 16:54:28 -0300

    html do arquivo de listagem

--- listagem.php
+++ listagem.php
@@ -1,15 +1,53 @@
 <?php
+include 'conexao.php';
 
-$servidor = 'localhost';
-$usuario = 'root';
-$senha = '';
-$nome_banco = 'veterinaria';
+$buscar_cadastros = "SELECT * FROM cliente";//seleciona tudo da tabela cliente
 
-$conexao = mysqli_connect($servidor, $usuario, $senha, $nome_banco);
 
-if($conexao){
-    echo 'Conectou';
-}else{
-    echo 'erro';
-}
+
+
 ?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Cadastro de Clientes</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <table class="table">
+        <thead><!--cabeçalho -->
+            <tr><!--cada tr é uma linha -->
+                <th>Cpf</th>
+                <th>Nome</th>
+                <th>Telefone</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td scope="row">1</td>
+                <td>ricardo</td>
+                <td>111</td>
+            </tr>
+            <tr>
+                <td scope="row">2</td>
+                <td>laura</td>
+                <td>222</td>
+            </tr>
+        </tbody>
+    </table>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
